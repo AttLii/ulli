b344a90ed3c78f46708b8074cb76f8a729d63a8b
Atte Liimatainen <atte.liimat@gmail.com>
2021-08-17 13:15:34 +0300

    add build command
    - change main file to dist file

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import { Ulli } from "./components/Ulli";
 
 export default function App() {
   return (
-    <>
-      <Ulli
-        ListElement="ol"
-        className="hi"
-        itemClassName="hiho"
-        list={["item1", "item2"]}
-        itemFunc={(item, i) => (
-          <p>
-            {i} {item}
-          </p>
-        )}
-      />
-    </>
+    <Ulli
+      ListElement="ol"
+      className="hi"
+      itemClassName="hiho"
+      list={["item1", "item2"]}
+      itemFunc={(item, i) => (
+        <p>
+          {i} {item}
+        </p>
+      )}
+    />
   );
 }
